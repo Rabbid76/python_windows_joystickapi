9335ac1cb0a7ff204654e33330899e4ef2b3e3f3
Rabbid76 <Gernot.Steinegger@gmail.com>
2020-02-20 16:15:21 +0100

    exception

--- joystickapi.py
+++ joystickapi.py
@@ -1,5 +1,75 @@
 import ctypes
 
+
+try:
+    winmmdll = ctypes.WinDLL('winmm.dll')
+
+    # [joyGetNumDevs](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetnumdevs)
+    """
+    UINT joyGetNumDevs();
+    """
+    joyGetNumDevs_proto = ctypes.WINFUNCTYPE(ctypes.c_uint)
+    joyGetNumDevs_func  = joyGetNumDevs_proto(("joyGetNumDevs", winmmdll))
+
+    # [joyGetDevCaps](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetdevcaps)
+    """
+    MMRESULT joyGetDevCaps(UINT uJoyID, LPJOYCAPS pjc, UINT cbjc);
+
+    32 bit: joyGetDevCapsA
+    64 bit: joyGetDevCapsW
+
+    sizeof(JOYCAPS): 728
+    """
+    joyGetDevCaps_proto = ctypes.WINFUNCTYPE(ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint)
+    joyGetDevCaps_param = (1, "uJoyID", 0), (1, "pjc", None), (1, "cbjc", 0)
+    joyGetDevCaps_func  = joyGetDevCaps_proto(("joyGetDevCapsW", winmmdll), joyGetDevCaps_param)
+
+    # [joyGetPosEx](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetposex)
+    """
+    MMRESULT joyGetPosEx(UINT uJoyID, LPJOYINFOEX pji);
+    sizeof(JOYINFOEX): 52
+    """
+    joyGetPosEx_proto = ctypes.WINFUNCTYPE(ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p)
+    joyGetPosEx_param = (1, "uJoyID", 0), (1, "pji", None)
+    joyGetPosEx_func  = joyGetPosEx_proto(("joyGetPosEx", winmmdll), joyGetPosEx_param)
+except:
+    winmmdll = None
+
+# joystickapi - joyGetNumDevs
+def joyGetNumDevs():
+    try:
+        num = joyGetNumDevs_func()
+    except:
+        num = 0
+    return num
+
+# joystickapi - joyGetDevCaps
+def joyGetDevCaps(uJoyID):
+    try:
+        buffer = (ctypes.c_ubyte * JOYCAPS.SIZE_W)()
+        p1 = ctypes.c_uint(uJoyID)
+        p2 = ctypes.cast(buffer, ctypes.c_void_p)
+        p3 = ctypes.c_uint(JOYCAPS.SIZE_W)
+        ret_val = joyGetDevCaps_func(p1, p2, p3)
+        ret = (False, None) if ret_val != JOYERR_NOERROR else (True, JOYCAPS(buffer))   
+    except:
+        ret = False, None
+    return ret 
+
+# joystickapi - joyGetPosEx
+def joyGetPosEx(uJoyID):
+    try:
+        buffer = (ctypes.c_uint32 * (JOYINFOEX.SIZE // 4))()
+        buffer[0] = JOYINFOEX.SIZE
+        buffer[1] = JOY_RETURNALL
+        p1 = ctypes.c_uint(uJoyID)
+        p2 = ctypes.cast(buffer, ctypes.c_void_p)
+        ret_val = joyGetPosEx_func(p1, p2)
+        ret = (False, None) if ret_val != JOYERR_NOERROR else (True, JOYINFOEX(buffer))   
+    except:
+        ret = False, None
+    return ret 
+
 JOYERR_NOERROR = 0
 JOY_RETURNX = 0x00000001
 JOY_RETURNY = 0x00000002
@@ -17,37 +87,7 @@ JOY_RETURNALL = (JOY_RETURNX | JOY_RETURNY | JOY_RETURNZ | \
 				 JOY_RETURNR | JOY_RETURNU | JOY_RETURNV | \
 				 JOY_RETURNPOV | JOY_RETURNBUTTONS)
 
-winmmdll = ctypes.WinDLL('winmm.dll')
-
-# [joyGetNumDevs](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetnumdevs)
-"""
-UINT joyGetNumDevs();
-"""
-joyGetNumDevs_proto = ctypes.WINFUNCTYPE(ctypes.c_uint)
-joyGetNumDevs_func  = joyGetNumDevs_proto(("joyGetNumDevs", winmmdll))
-
-# [joyGetDevCaps](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetdevcaps)
-"""
-MMRESULT joyGetDevCaps(UINT uJoyID, LPJOYCAPS pjc, UINT cbjc);
-
-32 bit: joyGetDevCapsA
-64 bit: joyGetDevCapsW
-
-sizeof(JOYCAPS): 728
-"""
-joyGetDevCaps_proto = ctypes.WINFUNCTYPE(ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint)
-joyGetDevCaps_param = (1, "uJoyID", 0), (1, "pjc", None), (1, "cbjc", 0)
-joyGetDevCaps_func  = joyGetDevCaps_proto(("joyGetDevCapsW", winmmdll), joyGetDevCaps_param)
-
-# [joyGetPosEx](https://docs.microsoft.com/en-us/windows/win32/api/joystickapi/nf-joystickapi-joygetposex)
-"""
-MMRESULT joyGetPosEx(UINT uJoyID, LPJOYINFOEX pji);
-sizeof(JOYINFOEX): 52
-"""
-joyGetPosEx_proto = ctypes.WINFUNCTYPE(ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p)
-joyGetPosEx_param = (1, "uJoyID", 0), (1, "pji", None)
-joyGetPosEx_func  = joyGetPosEx_proto(("joyGetPosEx", winmmdll), joyGetPosEx_param)
-
+# joystickapi - JOYCAPS
 class JOYCAPS:
     SIZE_W = 728
     OFFSET_V = 4 + 32*2
@@ -64,6 +104,7 @@ class JOYCAPS:
         self.wRmin, self.wRmax, self.wUmin, self.wUmax, self.wVmin, self.wVmax, \
         self.wCaps, self.wMaxAxes, self.wNumAxes, self.wMaxButtons = uint_array
 
+# joystickapi - JOYINFOEX
 class JOYINFOEX:
   SIZE = 52
   def __init__(self, buffer):
@@ -71,36 +112,3 @@ class JOYINFOEX:
       self.dwSize, self.dwFlags, \
       self.dwXpos, self.dwYpos, self.dwZpos, self.dwRpos, self.dwUpos, self.dwVpos, \
       self.dwButtons, self.dwButtonNumber, self.dwPOV, self.dwReserved1, self.dwReserved2 = uint_array
-
-def joyGetNumDevs():
-    try:
-        num = joyGetNumDevs_func()
-    except:
-        num = 0
-    return num
-
-def joyGetDevCaps(uJoyID):
-    try:
-        buffer = (ctypes.c_ubyte * JOYCAPS.SIZE_W)()
-        p1 = ctypes.c_uint(uJoyID)
-        p2 = ctypes.cast(buffer, ctypes.c_void_p)
-        p3 = ctypes.c_uint(JOYCAPS.SIZE_W)
-        ret_val = joyGetDevCaps_func(p1, p2, p3)
-        ret = (False, None) if ret_val != JOYERR_NOERROR else (True, JOYCAPS(buffer))   
-    except:
-        ret = False, None
-    return ret 
-
-def joyGetPosEx(uJoyID):
-    try:
-        buffer = (ctypes.c_uint32 * (JOYINFOEX.SIZE // 4))()
-        buffer[0] = JOYINFOEX.SIZE
-        buffer[1] = JOY_RETURNALL
-        p1 = ctypes.c_uint(uJoyID)
-        p2 = ctypes.cast(buffer, ctypes.c_void_p)
-        ret_val = joyGetPosEx_func(p1, p2)
-        ret = (False, None) if ret_val != JOYERR_NOERROR else (True, JOYINFOEX(buffer))   
-    except:
-        ret = False, None
-        print("except")
-    return ret 
